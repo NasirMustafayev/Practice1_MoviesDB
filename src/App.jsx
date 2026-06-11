a928174f9deaaa3cae922c33ce32400a6b388161
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-06-11 12:24:47 +0400

    feat: update .gitignore to include environment files and refactor App component to use dynamic API key; add MovieDetail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { useState, useEffect, use} from 'react'
+import { useState, useEffect} from 'react'
 import { Grid, Container, Typography, ThemeProvider, CssBaseline } from "@mui/material";
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import MovieCard from "./MovieCard";
 import theme from "./theme"
 
+const api_key = import.meta.env.VITE_API_KEY
+const api_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=1`
+
 function App() {
-  const api_key = "b76733de3107af4735863080d6012c03"
-  const api_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=1`
   const [movies, setMovies] = useState([])
 
-  useEffect(() =>{
-
+//Will work without useEffect but it will cause infinite loop of fetching data and updating state
+//You can try to remove useEffect and see the console, you will see that it is fetching data and updating state infinitely
+useEffect(() => {
     fetch(api_url)
     .then(response => response.json())
-    .then(data =>  setMovies(data.results))
+    .then(data => {setMovies(data.results); console.log(data.results)})
     .catch(err => console.error('Errors:', err))
-  }, [])
+}, [])
 
   return(
     <ThemeProvider theme={theme}>
-    <CssBaseline>
+    <CssBaseline/>
     <Navbar/>
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Typography variant="h5">
@@ -35,7 +37,6 @@ function App() {
       </Grid>
     </Container>
     <Footer/>
-    </CssBaseline>
     </ThemeProvider>
   );
 }
